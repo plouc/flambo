446bb404c91f26acbfd4fc96f186548e88cb84e2
Raphael Benitte <benitte@ekino.com>
2016-06-04 18:22:53 +0900

    chore(docker): check for elastic & rethinkdb availability at startup

---
+++ api/initIndex.js
@@ -0,0 +1,73 @@
+const util      = require('util');
+const dotenv    = require('dotenv');
+dotenv.config({ silent: true });
+const container = require('./src/container');
+
+const newsItemMapping = require('./elastic/mappings/news_item');
+
+const es = container.get('elastic');
+
+const getMapping = () => {
+    return new Promise((resolve, reject) => {
+        es.indices.getMapping({
+            index: 'flambo',
+        }, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+};
+
+
+const putMapping = (type, mapping) => {
+    return new Promise((resolve, reject) => {
+        es.indices.putMapping({
+            index: 'flambo',
+            type,
+            body: mapping,
+        }, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+};
+
+const createIndex = () => {
+    return new Promise((resolve, reject) => {
+        es.indices.create({
+            index: 'flambo'
+        }, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+};
+
+createIndex()
+    .then(res => {
+        console.log(res);
+        return putMapping('news_item', newsItemMapping);
+    })
+    .then(res => {
+        console.log(res);
+        return getMapping();
+    })
+    .then(res => {
+        console.log(util.inspect(res.flambo.mappings, {
+            depth:  null,
+            colors: true,
+        }));
+    })
+    .catch(err => {
+        console.error(err);
+    })
+;
--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
+
 cd /api
 
 echo "installing node modules"
-npm install -q
+npm install
 
+echo "waiting for rethinkdb..."
+until curl -s "$RETHINKDB_HOST:8080" > /dev/null; do
+    echo "rethinkdb is unavailable - sleeping"
+    sleep 1
+done
+echo "rethinkdb is ready!"
+
+echo "initialize DB"
 node initSchema.js
-
 node loadData.js
+
+echo "waiting for elastic..."
+until curl -s "$ELASTIC_HOST:$ELASTIC_PORT" > /dev/null; do
+    echo "elastic is unavailable - sleeping"
+    sleep 1
+done
+echo "elastic is ready!"
+
+echo "initialize elastic index"
+node initIndex.js
+node collect.js
 
 nodemon -L api.js
